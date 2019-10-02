4cfcfbfb9d42e38ba03b20270582832ea2eae25d
Sam Lee <samleenz@me.com>
2019-10-02 15:11:54 +1000

    initial commit

---
+++ data-raw/g2p.R
@@ -0,0 +1,9 @@
+## code to prepare `g2p` dataset goes here
+
+# usethis::use_data("g2p")
+
+# 2019-10-02
+# This code will be to create a hgnc symbol -> uniprot ID
+# map that can easily be updated periodically.
+
+# it should also log when the file was last created
