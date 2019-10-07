ff40202363b6382f99a4c31e5745159796134652
Nathan Williams <nwilliams@svi.edu.au>
2019-10-07 15:32:22 +1100

    Uploaded disease2gene directory

---
+++ data-raw/disease2Gene/info.md
@@ -0,0 +1,15 @@
+# Info 
+
+Generates .json dict with diseases as keys and a hierarchical structure showing associated genes with their respective source, confidence and type
+
+Type can be text (via text mining), knowledge (curated) or Experimental (Gene Wide Association Studies)
+
+Source refers to the source of the information. FOr text this is simply just "text"
+
+Confidence is out of 5 and consistent across all types
+ 
+Input must be 3 .tsv files obtained from https://diseases.jensenlab.org/Downloads (USE FILTERED VERSIONS)
+ 
+Output .json saved as "disease2gene.json" in current directory
+
+Bonus Search function also provided but not used (for future applications)
---
+++ data-raw/disease2Gene/makeDB.py
@@ -0,0 +1,80 @@
+import csv
+import json
+import difflib
+
+#change these to updated files when necessary
+textMineFile = "human_disease_textmining_filtered.tsv"
+knowledgeFile = "human_disease_knowledge_filtered.tsv"
+experimentsFile = "human_disease_experiments_filtered.tsv"
+
+# Generates list of diseases in dictionary close to input query
+# Input: Search query (str), disease dict from makeDict(), maximum search results to output (default 3)
+# Output: List of matching queries (length up to set max) [MAY BE EMPTY IF NO MATCHES]
+def search(query, dic, maxResults=3):
+    keys = list(dic.keys())
+    return difflib.get_close_matches(query,keys,n=maxResults)
+
+# Generates central dictionary containing disease to gene associations with highest confidence source for each gene
+# Input: 3 filenames of filtered .tsv files from https://diseases.jensenlab.org/Downloads
+# Output: Dictionary with diseases as keys
+def makeDict(text,knowledge,experiments):
+    dic = {}
+
+    # add text file to dict with correct indexes
+    with open(text, 'r') as f:
+        reader = csv.reader(f, delimiter='\t')
+        for r in reader:
+            disease = r[3]
+            if disease in dic:
+                dic[disease][r[0]] = {}
+            else:
+                dic[disease] = {}
+                dic[disease][r[0]] = {}
+            dic[disease][r[0]]["confidence"] = r[5]
+            dic[disease][r[0]]["type"] = "text"
+            dic[disease][r[0]]["source"] = "textMine"
+
+    #add experiments file to dictionary with correct indexes and checking for previous entries
+    with open(experiments, 'r') as f:
+        reader = csv.reader(f, delimiter='\t')
+        for r in reader:
+            disease = r[3]
+            if disease in dic:
+                #take the higher score if conflict
+                if (r[1] in dic[disease] and r[6] < dic[disease][r[1]]["confidence"]):
+                    continue
+                dic[disease][r[1]] = {}
+            else:
+                dic[disease] = {}
+                dic[disease][r[1]] = {}
+            dic[disease][r[1]]["confidence"] = r[6]
+            dic[disease][r[1]]["type"] = "experiments"
+            dic[disease][r[1]]["source"] = r[4]
+
+    #add knowledge file to dictionary with correct indexes and checking for previous entries
+    with open(knowledge, 'r') as f:
+        reader = csv.reader(f, delimiter='\t')
+        for r in reader:
+            disease = r[3]
+            if disease in dic:
+                # take the higher score if conflict
+                if (r[1] in dic[disease] and r[6] < dic[disease][r[1]]["confidence"]):
+                    continue
+                dic[disease][r[1]] = {}
+
+            else:
+                dic[disease] = {}
+                dic[disease][r[1]] = {}
+            dic[disease][r[1]]["confidence"] = r[6]
+            dic[disease][r[1]]["type"] = "knowledge"
+            dic[disease][r[1]]["source"] = r[4]
+
+    #return the processed dictionary
+    return dic
+
+# Create central dict
+master = makeDict(textMineFile, knowledgeFile, experimentsFile)
+
+# save file to .json in current directory
+json.dump(master,open("disease2ene.json",'w'))
+
