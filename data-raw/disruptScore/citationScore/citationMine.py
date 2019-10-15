8d5527167485c8b62095809342a19f3b7da32054
Nathan Williams <nwilliams@svi.edu.au>
2019-10-16 10:54:54 +1100

    Added citation mining script and .json output after testing

---
+++ data-raw/disruptScore/citationScore/citationMine.py
@@ -0,0 +1,69 @@
+import json
+import requests
+import xmltodict
+
+def postProtList(prots):
+    protStr = ",".join(prots)
+    apiKey = "71d9e183e47693926dd1673d407f5bad0608"
+    emailAddr = "nwilliams@svi.edu.au"
+    data = {'db': 'protein', 'id': protStr, 'retmode': 'xml', 'email': emailAddr, "api_key": apiKey}
+    url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
+    store = {}
+
+    while True:
+        print("posting")
+        try:
+            r = requests.post(url,data)
+        except:
+            print("Error posting proteins, retrying...")
+            continue
+        print("parsing")
+        try:
+            parsed = xmltodict.parse(r.content)
+            print(r.status_code, r.reason)
+            mark = parsed["GBSet"]["GBSeq"]
+        except:
+            print("error parsing returned data, retrying...")
+            continue
+        break
+
+    for n in range(0,len(prots)):
+        store[prots[n]] = len(mark[n]["GBSeq_references"]["GBReference"])
+
+    return store
+
+def idListRead(fName):
+    idList = open(fName).read().splitlines()
+    return idList
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+
+def citeMine(fileName):
+    nSize = 100
+
+    ProtIds = idListRead(fileName)
+
+    pChunk = list(chunks(ProtIds,nSize))
+
+    master = {}
+
+    count = 0
+
+    for x in pChunk:
+        master.update(postProtList(x))
+        count += 1
+        complete = (count/len(pChunk)) * 100
+        print("\n" + "{:.4f}".format(complete) + "% Citations Complete\n")
+
+    json.dump(master,open("proteinCitations.json","w"))
+
+    return master
+
+print("Program Start")
+dic = citeMine("../../nedd_3graph_protein_union.txt")
+print(dic)
+
