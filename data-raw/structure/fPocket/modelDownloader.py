07c395d72465e49ac3c110d03c57d11ff87a7af5
Nathan Williams <nwilliams@svi.edu.au>
2019-10-23 11:27:29 +1100

    Added fPocket druggability estimation and model storage disposal. Also added better .json update functions

--- data-raw/structure/fPocket/modelDownloader.py
+++ data-raw/structure/fPocket/modelDownloader.py
@@ -1,13 +1,16 @@
 import os
 import json
-from threading import Thread
+from threading import Thread, get_ident
 import wget
+import shutil
 
 def process_id(p,models):
     # process a single ID
     out = os.path.abspath("structures/")
     pdbURL = "https://files.rcsb.org/download/"  # + "4hhb.pdb"
     swissURL = "https://swissmodel.expasy.org/repository/uniprot/"  # + "P07900.pdb"
+
+    print(p)
 
     if (models[p]["identity"] >= 40):
         # PDB
@@ -26,7 +29,7 @@ def process_id(p,models):
             return True
     return False
 
-def process_range(id_range, models, store=None, ):
+def process_range(id_range, models, store=None):
     """process a number of ids, storing the results in a dict"""
     if store is None:
         store = {}
@@ -51,28 +54,118 @@ def threaded_process_range(id_range, models, nthreads=1):
     [t.join() for t in threads]
     return store
 
-def downloadUpdates(old, new):
+def downloadUpdates(old, new, threads = 30):
     toDo = {}
 
     #add new or better structures to list
     for prot in new:
-        if(prot in curr):
-            if(new[prot]["identity"] > curr[prot]["identity"]):
+        if(prot in old):
+            if(new[prot]["identity"] > old[prot]["identity"]):
                 toDo[prot] = new[prot]
         else:
             toDo[prot] = new[prot]
 
     #download over N threads
-    output = threaded_process_range(list(toDo.keys()), toDo, 25)
+    output = threaded_process_range(list(toDo.keys()), toDo, threads)
 
     json.dump(output, open("output.json",'w'))
+
+    return output
+
+def process_models(targets):
+    for x in targets:
+        print(x + " ", end='')
+        print(str(get_ident()))
+        os.system('fpocket -f ./structures/' + str(x).upper() + '.pdb')
+
+def fPock(targets, nthreads):
+    threads = []
+    for i in range(nthreads):
+        ids = targets[i::nthreads]
+        t = Thread(target=process_models, args=(ids,))
+        threads.append(t)
+
+    # start the threads
+    [t.start() for t in threads]
+    # wait for the threads to finish
+    [t.join() for t in threads]
+
+def extractDruggabiliy():
+    curr = os.path.abspath("structures/")
+    resDict = {}
+
+    for filename in os.listdir(curr):
+        if (filename.endswith("_out")):
+            txt = os.path.abspath("structures/" + filename + '/' + filename[:-4] + '_info.txt')
+            txtLs = open(txt, 'r').read().splitlines()
+            drugScores = []
+            for line in txtLs:
+                if (line.startswith("	Druggability Score :")):
+                    drugScores.append(float(line[22:28]))
+
+            if (len(drugScores) > 0):
+                resDict[filename[:-4]] = max(drugScores)
+            else:
+                resDict[filename[:-4]] = 0
+
+    #delete all models
+    folder = './structures/'
+    for the_file in os.listdir(folder):
+        file_path = os.path.join(folder, the_file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path): shutil.rmtree(file_path)
+        except Exception as e:
+            print(e)
+
+    return resDict
+
+def compileMaster(drugResults, updates):
+    identities = json.load(open("../structureIdentity/structureIdentities.json",'r'))
+    json.dump(identities,open("currentModels.json",'w')) #update
+    master = json.load(open("../singleProtStats.json","r"))
+
+    for prot in updates:
+        if(prot not in master):
+            master[prot] = {}
+
+        if(prot in drugResults):
+            master[prot]["druggability"] = drugResults[prot]
+        else:
+            master[prot]["druggability"] = False
+        master[prot]["identity"] = identities[prot]["identity"]
+
+    json.dump(master,open("../singleProtStats.json","w"))
+    return master
+
+
 
 curr = json.load(open("currentModels.json",'r'))
 new = json.load(open("../structureIdentity/structureIdentities.json",'r'))
 
 print("begin")
 
-downloadUpdates(curr,new)
+out = downloadUpdates(curr,new)
+
+fPockTargets = [x for x in out if out[x]]
+
+fPock(fPockTargets, 4)
+
+druggability = extractDruggabiliy()
+
+updates = []
+
+
+for x in new:
+    if (x in curr):
+        if(new[x]["identity"] > curr[x]["identity"]):
+            updates.append(x)
+    else:
+        updates.append(x)
+
+#compileMaster
+compileMaster(druggability, updates)
 
 print("program complete")
 
