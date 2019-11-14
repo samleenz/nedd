8f664466802e64f324119dadc66531a47aa76b16
Sam Lee <samleenz@me.com>
2019-11-14 12:11:14 +1100

    initial commit of zero to one range scaling utility

--- R/scores.R
+++ R/scores.R
@@ -1,3 +1,47 @@
+#' Zero to One: range scaling
+#'
+#' Utility function: converts a numeric vector to the range [0,1] or (0,1)
+#'
+#' @param x Numeric vector to be scaled
+#' @param incZero Logical: whether the bottom of the interval should be open or closed
+#'
+#' @return the rescaled vector x, the range will be [0,1] or (0,1) depending on the value of \code{incZero}
+#' @export
+#'
+#' @examples
+z2o <- function(x, incZero = TRUE){
+
+  # check length > 0
+  if(! length(x) > 0){
+    stop("x must have length > 0")
+  }
+
+  # check if x is a numeric vector
+  if(! is.numeric(x)){
+    stop("x must be numeric")
+  }
+
+  # check incZero is logical
+
+  if(! is.logical(incZero)){
+    stop("incZero must be a logical")
+  }
+
+
+  if(! isTRUE(incZero)){
+    x <- c(0, x)
+  }
+
+  # rearrange scale vector x from 0 to 1
+  x_ret <- (x-min(x))/(max(x)-min(x))
+
+  if(! isTRUE(incZero)){
+    x_ret <- x_ret[-1]
+  }
+
+  return(x_ret)
+}
+
 #' Filter scores
 #'
 #' Filter a set of scores (i.e. a 2 col dataframe) to remove scored
--- tests/testthat/test-scores.R
+++ tests/testthat/test-scores.R
@@ -1,3 +1,22 @@
-test_that("Filter scores works", {
+test_that("Inputs are of the correct type", {
   # do things
+  expect_error(
+    z2o("cat", incZero = TRUE),
+    "x must be numeric"
+    )
+  expect_error(
+    z2o(c(1,5,2), incZero = "cat"),
+    "incZero must be a logical"
+  )
+})
+
+
+test_that("Scaling is correct", {
+  vec <- c(sample(1:50, 10))
+
+  scaled <- z2o(vec)
+
+  scaled_manual <- (vec-min(vec))/(max(vec)-min(vec))
+
+  expect_equal(scaled, scaled_manual)
 })
