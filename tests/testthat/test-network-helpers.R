a25e75169a5f7850b17c0f4ad7ca9c689e918e80
Sam Lee <samleenz@me.com>
2019-11-25 12:23:33 +1100

    updated tests for netStats

--- tests/testthat/test-network-helpers.R
+++ tests/testthat/test-network-helpers.R
@@ -1,3 +1,6 @@
+# netStats tests ----------------------------------------------------------
+g <- igraph::barabasi.game(5, directed = FALSE)
+
 test_that("g is a graph", {
   expect_error(
     netStats("cat", norm = FALSE),
@@ -7,8 +10,25 @@ test_that("g is a graph", {
 
 test_that("norm is a logical", {
   expect_error(
-    netStats(igraph::barabasi.game(5), "cat"),
+    netStats(g, "cat"),
     "norm must be a logical"
   )
 })
 
+test_that("Drug scores are NA if requested and missing", {
+  input_na   <- c("goo", "foo", "bar", "cat", "dog")
+  expect_equal(
+    netStats(igraph::set_vertex_attr(g, "name", value = input_na))$drug_score,
+    rep(NA, 5)
+    )
+})
+
+test_that("Returned values are numeric", {
+  expect_true(
+    all(apply(
+      netStats(g, named = FALSE),
+      2,
+      is.numeric
+    ))
+  )
+})
