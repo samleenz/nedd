81b9e817c127f50b483bbfa17a74674826fe9083
Sam Lee <samleenz@me.com>
2020-01-28 11:40:40 +1100

    initial commit, four functions to write and to test

---
+++ R/differential.R
@@ -0,0 +1,47 @@
+#' Title
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+diff_n <- function(x){
+
+}
+
+#' Title
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+pref_n <- function(x){
+
+}
+
+#' Title
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+pref_i <- function(x){
+
+}
+
+#' Title
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+diff_i <- function(x){
+
+}
