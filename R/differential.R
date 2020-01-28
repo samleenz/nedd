2058cf610cbdc40face23b131124cfbbd7fc438c
Sam Lee <samleenz@me.com>
2020-01-28 12:52:29 +1100

    initial code for diff_n and wrote strip_attr helper function. also some minor documentation additions

--- R/differential.R
+++ R/differential.R
@@ -1,16 +1,52 @@
-#' Title
+#' Differential node weighting
 #'
-#' @param x
+#' Set of differential network methods taken from O. Basha et al., Bioinformatics (2020).
 #'
-#' @return
+#' Takes two networks that differ only in  node weights (canonically log2 expression).
+#' \code{diff_n} calculates the weight \code{n} of the output network as the difference between the weight in network one to that in network two.
+#'  In the case of a multigroup comparison (classes > 2) the second network node weights should be the median of each class's weights.
+#'
+#' @param g1 a node weighted igraph network
+#' @param g2 a node weighted igraph network
+#'
+#' @return a network where the node weights reprsent the difference in node weights from the two input networks.
 #' @export
 #'
 #' @examples
-diff_n <- function(x){
+diff_n <- function(g1, g2, name1 = "weight", name2 = "weight", nameOut = "weight"){
+
+  # test that nodes and edges of g1 and g2 are the same
+  if(! igraph::identical_graphs(
+    nedd::strip_vertex_attr(g1),
+    nedd::strip_vertex_attr(g2)
+  )) {
+    stop("input graphs must have the same nodes and edges")
+  }
+
+  n1 <- igraph::vertex_attr(g1, name1)
+  n2 <- igraph::vertex_attr(g2, name2)
+
+  # test that weights are numeric
+  if(! is.numeric(c(n1, n2))){
+    stop("node weights must be numeric")
+  }
+
+
+  nOut <- n1 - n2
+
+  gOut <- g1
+  igraph::vertex_attr(gOut, nameOut) <- nOut
+
+  return(gOut)
 
 }
 
-#' Title
+
+#' Preferential node weighting
+#'
+#' Set of differential network methods taken from O. Basha et al., Bioinformatics (2020).
+#' \code{pref_n} calculate the preferential expression of a node as described by
+
 #'
 #' @param x
 #'
@@ -20,9 +56,13 @@ diff_n <- function(x){
 #' @examples
 pref_n <- function(x){
 
+
 }
 
-#' Title
+
+#' Preferntial edge weighting
+#'
+#' Set of differential network methods taken from O. Basha et al., Bioinformatics (2020).
 #'
 #' @param x
 #'
@@ -34,7 +74,10 @@ pref_i <- function(x){
 
 }
 
-#' Title
+
+#' Differential interaction weighting
+#'
+#' Set of differential network methods taken from O. Basha et al., Bioinformatics (2020).
 #'
 #' @param x
 #'
@@ -44,4 +87,27 @@ pref_i <- function(x){
 #' @examples
 diff_i <- function(x){
 
+}
+
+#' Strip graph attributes
+#'
+#' removes all vertex and edge attributes from a graph.
+#' internal function for prepping graphs to test if structures are the same
+#'
+#' @param g
+#'
+#' @return the graph g with al vertex attributes removed
+#'
+strip_attr <- function(g){
+
+  gOut <- g
+  for(name in igraph::vertex_attr_names(gOut)){
+    gOut <- igraph::delete_vertex_attr(gOut, name)
+  }
+
+  for(name in igraph::edge_attr_names(gOut)){
+    gOut <- igraph::delete_edge_attr(gOut, name)
+  }
+
+  return(gOut)
 }
