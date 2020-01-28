347b2dd8db17b1a9c2821fc1a6bc68e6c0952cfe
Sam Lee <samleenz@me.com>
2020-01-28 16:07:19 +1100

    change strip_atrr test to testing equality of vertexes and edges independently, what could this possibly miss?

--- tests/testthat/test-differential.R
+++ tests/testthat/test-differential.R
@@ -5,10 +5,14 @@ test_that("strip_attr works", {
   igraph::vertex_attr(g2, "weight") <- sample(igraph::vcount(g2))
   igraph::edge_attr(g2, "weight") <- sample(igraph::ecount(g2))
 
+  g2 <- nedd:::strip_attr(g2)
+
   expect_true(
-    igraph::isomorphic(
-      g,
-      nedd:::strip_attr(g2)
-    )
+    all(igraph::V(g) == igraph::V(g2)) & all(igraph::E(g) == igraph::E(g2))
   )
+
 })
+
+# test_that("Input graphs are isomorphic"){
+#
+# }
