f98ee16b73e31cec03a891ff5ecb313988a9b0d5
Sam Lee <samleenz@me.com>
2020-01-30 10:41:55 +1100

    added igraph namespace to functions

--- tests/testthat/test-differential.R
+++ tests/testthat/test-differential.R
@@ -17,12 +17,12 @@ test_that("diff_n gives expected result", {
   g <- igraph::barabasi.game(15, directed = F)
   g1 <- g
   g2<- g
-  vertex_attr(g1, "weight") <- sample(50, vcount(g))
-  vertex_attr(g2, "weight") <- sample(50, vcount(g))
+  igraph::vertex_attr(g1, "weight") <- sample(50, igraph::vcount(g))
+  igraph::vertex_attr(g2, "weight") <- sample(50, igraph::vcount(g))
 
   gdiff <- nedd::diff_n(g1, g2)
 
 
 
-  expect_equal((V(g1)$weight - V(g2)$weight), V(nedd::diff_n(g1, g2))$weight)
+  expect_equal((igraph::V(g1)$weight - igraph::V(g2)$weight), igraph::V(nedd::diff_n(g1, g2))$weight)
 })
